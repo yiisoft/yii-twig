7f16dc1804d880dd9c8ac77622eb04503c536ee4
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-10 13:33:46 +0300

    Add Composer Dependency Analyser (#17)

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan(__DIR__ . '/config', isDev: false)
+    ->addPathToScan(__DIR__ . '/src', isDev: false)
+    ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    // `yiisoft/definitions` is used only in `config/params.php` and `config/params-web.php`, which are loaded
+    // by consumers using `yiisoft/di`, that already requires `yiisoft/definitions` itself.
+    ->ignoreErrorsOnPackages(['yiisoft/definitions'], [ErrorType::SHADOW_DEPENDENCY])
+    // `Yiisoft\Aliases\Aliases` is used only in `config/*`
+    ->ignoreUnknownClasses(['Yiisoft\Aliases\Aliases']);
